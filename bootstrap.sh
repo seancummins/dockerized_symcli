66587336654c574b0b9172f6d95fe959eb05c4aa
Sean Cummins <sean@scummins.com>
2014-11-20 00:42:58 -0500

    Added MOTD

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-apt-get update
+#apt-get update
+
+sudo rm /etc/update-motd.d/[0-8]?-*
+sudo rm /etc/update-motd.d/98-*
+sudo cp /vagrant/etc/55-symcli /etc/update-motd.d
+sudo chmod +x /etc/update-motd.d/55-symcli
 
 echo "alias se8='docker start se8 && docker attach se8'" >>/home/vagrant/.bashrc
 echo "alias se76='docker start se76 && docker attach se76'" >>/home/vagrant/.bashrc
